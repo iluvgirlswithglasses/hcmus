97a67fe36b98acd2b0c46435b50d0f94504d3c84
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-10-30 19:20:07 +0700

    added the template

---
+++ yr1/t.hcmus.cpp
@@ -0,0 +1,50 @@
+
+/*
+author: 	iluvgirlswithglasses 
+github: 	https://github.com/iluvgirlswithglasses 
+created:	{{date}}
+tab-width:	4 spaces
+
+ /\_/\
+( o.o )
+ > ^ <
+
+My template to make interactive cli apps in hcmus...
+...in beginner level
+*/
+
+#include <iostream>
+using namespace std;
+
+/** @ defs */
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ld, ld> pd;
+#define st first
+#define nd second
+
+/** @ constraints */
+const int N = 1e6+7, I = 2e9+7, R = 1e9+7;
+const ld  EPS = 1e-6;
+// const ld PI = cos(0) * 2;
+
+/** @ vars */
+
+/** @ utils */
+
+/** @ drivers */
+const char* GREETING_MESSAGE = \
+	"This program takes 2 arguments.\n" \
+	"First is `X`, which indicates abc.\n" \
+	"And the second is `Y`, which indicates xyz.\n" \
+	"Please type in the required arguments.\n";
+
+int main() {
+	// ios_base::sync_with_stdio(false); cin.tie(0);
+	cout << GREETING_MESSAGE;
+    // @ inp
+    // @ cal
+    // @ out
+	return 0;
+}
