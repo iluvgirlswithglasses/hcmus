8c6f09686e978bd38da58c805e6504d2dcd734d9
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-11-03 07:59:02 +0700

    template simplified

--- yr1/t.hcmus.cpp
+++ yr1/t.hcmus.cpp
@@ -9,40 +9,28 @@ tab-width:	4 spaces
 ( o.o )
  > ^ <
 
-My template to make interactive cli apps in hcmus...
-...in beginner level
+My template to make interactive cli apps in hcmus
+with no struct no enum no define no ptrs no anything
 */
 
 #include <iostream>
 using namespace std;
 
-/** @ defs */
-typedef long long ll;
-typedef long double ld;
-typedef pair<int, int> pi;
-typedef pair<ld, ld> pd;
-#define st first
-#define nd second
-
 /** @ constraints */
-const int N = 1e6+7, I = 2e9+7, R = 1e9+7;
-const ld  EPS = 1e-6;
-// const ld PI = cos(0) * 2;
-
-/** @ vars */
+const int    N = 1e6+7, I = 2e9+7, R = 1e9+7;
+const double EPS = 1e-6;
+// const double PI = cos(0) * 2;
 
 /** @ utils */
 
 /** @ drivers */
-const char* GREETING_MESSAGE = \
-	"This program takes 2 arguments.\n" \
-	"First is `X`, which indicates abc.\n" \
-	"And the second is `Y`, which indicates xyz.\n" \
-	"Please type in the required arguments.\n";
-
 int main() {
 	// ios_base::sync_with_stdio(false); cin.tie(0);
-	cout << GREETING_MESSAGE;
+	cout << \
+		"This program takes 2 arguments.\n" \
+		"First is `X`, which indicates abc.\n" \
+		"And the second is `Y`, which indicates xyz.\n" \
+		"Please type in the required arguments.\n";
     // @ inp
     // @ cal
     // @ out
